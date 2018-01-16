8c0334e7874ebd2ad77f476628ffaeb505319653
songyangguang <372752655@qq.com>
2018-01-16 16:15:54 +0800

    获取抓拍到的图片

---
+++ src/main/java/com/wsn/restful/api/device/GetPictureByUUIDApi.java
@@ -0,0 +1,9 @@
+package com.wsn.restful.api.device;
+
+import com.wsn.restful.api.AbstractAPI;
+
+/**
+ * Created by songyangguang on 2018/1/16.
+ */
+public class GetPictureByUUIDApi extends AbstractAPI {
+}
